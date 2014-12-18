439a9c1927393c72851a23f7507e12f8492ea082
Nick Robinson <nr33@msstate.edu>
2014-12-18 16:52:11 -0600

    Updating login page

--- sections/index/public.php
+++ sections/index/public.php
@@ -1,4 +1,3 @@
 <? View::show_header(); ?>
-<div class="poetry">You've stumbled upon a door where your mind is the key. There are none who will lend you guidance; these trials are yours to conquer alone. Entering here will take more than mere logic and strategy, but the criteria are just as hidden as what they reveal. Find yourself, and you will find the very thing hidden behind this page. Beyond here is something like a utopia&#8202;&mdash;&#8202;beyond here is What.CD.</div>
-<span class="center">This is a mirage.</span>
+<div class="poetry">Glad you could make it. We have been waiting for you. If you love radio then this is the site for you. Come in and checkout what we have to offer.</div>
 <? View::show_footer(); ?>
